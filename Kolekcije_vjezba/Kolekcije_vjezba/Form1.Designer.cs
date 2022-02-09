b2d489e5c7aa18302d14d8b952683a7effac5052
DavidTrstenjak <90246263+DavidTrstenjak@users.noreply.github.com>
2022-02-09 17:20:42 +0100

    Add files via upload

---
+++ Kolekcije_vjezba/Kolekcije_vjezba/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Kolekcije_vjezba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Unesi = new System.Windows.Forms.Button();
+            this.button_ispisi = new System.Windows.Forms.Button();
+            this.button_obradi = new System.Windows.Forms.Button();
+            this.label_podatak1 = new System.Windows.Forms.Label();
+            this.label_podatak2 = new System.Windows.Forms.Label();
+            this.label_podatak3 = new System.Windows.Forms.Label();
+            this.label_podatak4 = new System.Windows.Forms.Label();
+            this.textBox_podatak1 = new System.Windows.Forms.TextBox();
+            this.textBox_podatak3 = new System.Windows.Forms.TextBox();
+            this.textBox_podatak2 = new System.Windows.Forms.TextBox();
+            this.comboBox_podatak4 = new System.Windows.Forms.ComboBox();
+            this.richTextBox_ispis = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button_Unesi
+            // 
+            this.button_Unesi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button_Unesi.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_Unesi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Unesi.Location = new System.Drawing.Point(156, 12);
+            this.button_Unesi.Name = "button_Unesi";
+            this.button_Unesi.Size = new System.Drawing.Size(163, 156);
+            this.button_Unesi.TabIndex = 0;
+            this.button_Unesi.Text = "Unesi";
+            this.button_Unesi.UseVisualStyleBackColor = false;
+            this.button_Unesi.Click += new System.EventHandler(this.button_Unesi_Click);
+            // 
+            // button_ispisi
+            // 
+            this.button_ispisi.BackColor = System.Drawing.Color.SteelBlue;
+            this.button_ispisi.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_ispisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_ispisi.Location = new System.Drawing.Point(325, 12);
+            this.button_ispisi.Name = "button_ispisi";
+            this.button_ispisi.Size = new System.Drawing.Size(161, 156);
+            this.button_ispisi.TabIndex = 1;
+            this.button_ispisi.Text = "Ispiši";
+            this.button_ispisi.UseVisualStyleBackColor = false;
+            this.button_ispisi.Click += new System.EventHandler(this.button_ispisi_Click);
+            // 
+            // button_obradi
+            // 
+            this.button_obradi.BackColor = System.Drawing.Color.Lime;
+            this.button_obradi.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_obradi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_obradi.Location = new System.Drawing.Point(492, 11);
+            this.button_obradi.Name = "button_obradi";
+            this.button_obradi.Size = new System.Drawing.Size(166, 157);
+            this.button_obradi.TabIndex = 2;
+            this.button_obradi.Text = "Obradi";
+            this.button_obradi.UseVisualStyleBackColor = false;
+            this.button_obradi.Click += new System.EventHandler(this.button_obradi_Click);
+            // 
+            // label_podatak1
+            // 
+            this.label_podatak1.AutoSize = true;
+            this.label_podatak1.Location = new System.Drawing.Point(13, 12);
+            this.label_podatak1.Name = "label_podatak1";
+            this.label_podatak1.Size = new System.Drawing.Size(24, 13);
+            this.label_podatak1.TabIndex = 3;
+            this.label_podatak1.Text = "Ime";
+            // 
+            // label_podatak2
+            // 
+            this.label_podatak2.AutoSize = true;
+            this.label_podatak2.Location = new System.Drawing.Point(13, 52);
+            this.label_podatak2.Name = "label_podatak2";
+            this.label_podatak2.Size = new System.Drawing.Size(44, 13);
+            this.label_podatak2.TabIndex = 4;
+            this.label_podatak2.Text = "Prezime";
+            // 
+            // label_podatak3
+            // 
+            this.label_podatak3.AutoSize = true;
+            this.label_podatak3.Location = new System.Drawing.Point(13, 91);
+            this.label_podatak3.Name = "label_podatak3";
+            this.label_podatak3.Size = new System.Drawing.Size(80, 13);
+            this.label_podatak3.TabIndex = 5;
+            this.label_podatak3.Text = "Godina rođenja";
+            this.label_podatak3.Click += new System.EventHandler(this.label_podatak3_Click);
+            // 
+            // label_podatak4
+            // 
+            this.label_podatak4.AutoSize = true;
+            this.label_podatak4.Location = new System.Drawing.Point(13, 130);
+            this.label_podatak4.Name = "label_podatak4";
+            this.label_podatak4.Size = new System.Drawing.Size(28, 13);
+            this.label_podatak4.TabIndex = 6;
+            this.label_podatak4.Text = "Spol";
+            // 
+            // textBox_podatak1
+            // 
+            this.textBox_podatak1.Location = new System.Drawing.Point(13, 29);
+            this.textBox_podatak1.Name = "textBox_podatak1";
+            this.textBox_podatak1.Size = new System.Drawing.Size(124, 20);
+            this.textBox_podatak1.TabIndex = 7;
+            // 
+            // textBox_podatak3
+            // 
+            this.textBox_podatak3.Location = new System.Drawing.Point(13, 107);
+            this.textBox_podatak3.Name = "textBox_podatak3";
+            this.textBox_podatak3.Size = new System.Drawing.Size(124, 20);
+            this.textBox_podatak3.TabIndex = 8;
+            // 
+            // textBox_podatak2
+            // 
+            this.textBox_podatak2.Location = new System.Drawing.Point(12, 68);
+            this.textBox_podatak2.Name = "textBox_podatak2";
+            this.textBox_podatak2.Size = new System.Drawing.Size(124, 20);
+            this.textBox_podatak2.TabIndex = 10;
+            // 
+            // comboBox_podatak4
+            // 
+            this.comboBox_podatak4.FormattingEnabled = true;
+            this.comboBox_podatak4.Items.AddRange(new object[] {
+            "M",
+            "Ž"});
+            this.comboBox_podatak4.Location = new System.Drawing.Point(12, 147);
+            this.comboBox_podatak4.Name = "comboBox_podatak4";
+            this.comboBox_podatak4.Size = new System.Drawing.Size(125, 21);
+            this.comboBox_podatak4.TabIndex = 11;
+            // 
+            // richTextBox_ispis
+            // 
+            this.richTextBox_ispis.Location = new System.Drawing.Point(12, 174);
+            this.richTextBox_ispis.Name = "richTextBox_ispis";
+            this.richTextBox_ispis.Size = new System.Drawing.Size(646, 264);
+            this.richTextBox_ispis.TabIndex = 12;
+            this.richTextBox_ispis.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 450);
+            this.Controls.Add(this.richTextBox_ispis);
+            this.Controls.Add(this.comboBox_podatak4);
+            this.Controls.Add(this.textBox_podatak2);
+            this.Controls.Add(this.textBox_podatak3);
+            this.Controls.Add(this.textBox_podatak1);
+            this.Controls.Add(this.label_podatak4);
+            this.Controls.Add(this.label_podatak3);
+            this.Controls.Add(this.label_podatak2);
+            this.Controls.Add(this.label_podatak1);
+            this.Controls.Add(this.button_obradi);
+            this.Controls.Add(this.button_ispisi);
+            this.Controls.Add(this.button_Unesi);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Unesi;
+        private System.Windows.Forms.Button button_ispisi;
+        private System.Windows.Forms.Button button_obradi;
+        private System.Windows.Forms.Label label_podatak1;
+        private System.Windows.Forms.Label label_podatak2;
+        private System.Windows.Forms.Label label_podatak3;
+        private System.Windows.Forms.Label label_podatak4;
+        private System.Windows.Forms.TextBox textBox_podatak1;
+        private System.Windows.Forms.TextBox textBox_podatak3;
+        private System.Windows.Forms.TextBox textBox_podatak2;
+        private System.Windows.Forms.ComboBox comboBox_podatak4;
+        private System.Windows.Forms.RichTextBox richTextBox_ispis;
+    }
+}
+
---
+++ Kolekcije_vjezba/Kolekcije_vjezba/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kolekcije_vjezba
+{
+    public partial class Form1 : Form
+    {
+        List<Osobe> osoba = new List<Osobe>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Unesi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Osobe osobe = new Osobe(textBox_podatak1.Text, textBox_podatak2.Text, textBox_podatak3.Text, comboBox_podatak4.Text);
+
+                osoba.Add(osobe);
+
+                textBox_podatak1.Clear();
+                textBox_podatak2.Clear();
+                textBox_podatak3.Clear();
+
+                MessageBox.Show("Uspješan unos!", "Obavijest", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception greska)
+            {
+                MessageBox.Show("Pogrešan unos!\r\n" + greska.Message, "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button_ispisi_Click(object sender, EventArgs e)
+        {
+            richTextBox_ispis.Text = "Podatak1 " + "\t\t" + "Podatak2" + "\t\t" + "podatak3" + "\t\t" + "podatak4" + "\t\t" + "Dodatak" + "\r\n";
+            foreach (Osobe osobe in osoba)
+            {
+                richTextBox_ispis.AppendText(osobe.ToString() + "\r\n");
+
+            }
+        }
+
+        private void button_obradi_Click(object sender, EventArgs e)
+        {
+            if (comboBox_podatak4.Text == "M")
+            {
+                richTextBox_ispis.AppendText("Ima brkove");
+            }
+
+            else
+            {
+                richTextBox_ispis.AppendText("Nema brkove");
+            }
+        }
+
+        private void label_podatak3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
